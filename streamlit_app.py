d5403be940549ed14896e152facfc5c3e865e3c6
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-13 15:19:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,10 @@ st.header('st.file_uploader')
 uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
-  dataframe = pd.read_csv(uploaded_file)
-  st.write(dataframe)
+  df = pd.read_csv(uploaded_file)
+  with st.subheader('DataFrame'):
+    st.write(df)
+  with st.subheader('Descriptive Statistics'):
+    st.write(df.describe())
 else:
   st.info('☝️ Upload a CSV file')
