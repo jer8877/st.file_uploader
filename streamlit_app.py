aa655ed2e820d820d2483065b356d90672ef32e8
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-13 14:50:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,11 @@
 import streamlit as st
 
+st.header('st.file_uploader')
+
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+  dataframe = pd.read_csv(uploaded_file)
+  st.write(dataframe)
+else:
+  st.info('☝️ Upload a CSV file')
